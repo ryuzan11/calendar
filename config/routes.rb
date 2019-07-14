a817d62d864b30b4bfb6e029d04191253d6ccfe1
Yuta Osaka <yusoac@gmail.com>
2019-07-14 15:19:35 +0900

    Groupsのroutes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
   devise_for :users
-  root 'users#edit'
-  resources :users, only: [:edit, :update]
+
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new'
+    get 'sign_up' => 'devise/registrations#new'
+    get 'edit' => 'devise/registrations#edit'
+    delete 'logout' => 'devise/sessions#destroy'
+  end
+
+  resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
