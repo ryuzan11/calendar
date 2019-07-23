d674337266d71fff1a1df5da2f3197eacfad6afc
Yuta Osaka <yusoac@gmail.com>
2019-07-23 17:53:46 +0900

    カレンダーの月移動機能を実装

--- app/javascript/components/calItem/Calendar.jsx
+++ app/javascript/components/calItem/Calendar.jsx
@@ -1,14 +1,23 @@
 import React from "react"
+import YearMonth from "./YearMonth"
 import Youbi from "./Youbi"
 import CellDay from "./CellDay"
 
 class Calendar extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      year: this.props.year,
-      month: this.props.month
-    }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     year: this.props.year,
+  //     month: this.props.month,
+  //   }
+  // }
+
+  handlePrevCalendar = (prevMonth) => {
+    this.props.onInputPrev(prevMonth)
+  }
+
+  handleNextCalendar = (nextMonth) => {
+    this.props.onInputNext(nextMonth)
   }
 
   handleDateOutput = (click_date) =>{
@@ -19,12 +28,17 @@ class Calendar extends React.Component {
     return (
       <React.Fragment>
         {/* <div className="member">メンバー</div> */}
-        <h4>{this.state.year}年{this.state.month}月</h4>
+        <YearMonth 
+          year={this.props.year} 
+          month={this.props.month} 
+          onPrevMonth={this.handlePrevCalendar}
+          onNextMonth={this.handleNextCalendar}
+        />
         <div className="calendar">
           <Youbi />
           <CellDay 
-            year={this.state.year} 
-            month={this.state.month} 
+            year={this.props.year} 
+            month={this.props.month}
             onTaskDate={this.handleDateOutput}
           />
         </div>
