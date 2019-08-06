81c21ac36648c26c65398709571a55fe39c26494
Yuta Osaka <yusoac@gmail.com>
2019-08-07 04:20:17 +0900

    Task削除

--- app/javascript/components/calItem/Cell.jsx
+++ app/javascript/components/calItem/Cell.jsx
@@ -1,7 +1,35 @@
 import React from "react";
-import CellDelete from "CellDelete";
+import CellDelete from "./CellDelete";
 
 class Cell extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      modal_class: "",
+    }
+  }
+
+  componentDidMount = () =>{
+    let modal_class = "modal-comment"
+    this.setState({
+      modal_class: modal_class
+      })
+    }
+
+  handleClickDelete = () => {
+    let modal_class = "modal-comment is-show is-animate"
+    this.setState({
+      modal_class: modal_class
+    })
+  }
+
+  handleModalCancel = (modal_comment) => {
+    let modal_class = modal_comment
+    this.setState({
+      modal_class: modal_class,
+    })
+  }
+
   // handleClickTask = (e) => {
   //   let click_task = []
   //   click_task.push(e.target.className.split(" ")[0],e.target.id,e.target.innerHTML);
@@ -12,10 +40,15 @@ class Cell extends React.Component {
     return(
       <div className={this.props.task.id + " task-cell"} 
            id={this.props.ymd}
+           onClick={this.handleClickDelete}
           //  onClick={this.handleClickTask}
       >
-      <CellDelete />
       {this.props.task.title}
+      <CellDelete 
+        task={this.props.task} 
+        modal_class={this.state.modal_class}
+        onModalCancel={this.handleModalCancel}
+      />
       </div>
     )
   };
