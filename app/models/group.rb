e2dab6b653e901f73c710a35d5054669c82f0184
Yuta Osaka <yusoac@gmail.com>
2019-08-10 17:01:09 +0900

    グループ削除機能実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
-  has_many :group_users
+  has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
   accepts_nested_attributes_for :group_users
+  has_many :tasks, dependent: :destroy
 end
