c51c239b81238350b2d186f8a15b2c8d9f903d66
Yuta Osaka <yusoac@gmail.com>
2019-08-12 22:56:24 +0900

    OtherNav表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.where(group_id: params[:group_id]).order('task_start ASC')
     @groups = current_user.groups
-    @other_users = @group.group_users.where.not(user_id: current_user)
+    @other_users = @group.users.where.not(id: current_user)
     @other_tasks = @tasks.where.not(user_id: current_user)
     respond_to do |format|
       format.html
--- app/javascript/components/taskItem/OtherNav.jsx
+++ app/javascript/components/taskItem/OtherNav.jsx
@@ -2,23 +2,32 @@ import React from "react";
 
 class OtherNav extends React.Component{
   render(){
+    console.log(this.props.other_users)
     return(
       <React.Fragment>
         <div className="row nav">
           <ul className="nav nav-tabs task">
-            <li className="nav-item show">
-              <div className="nav-link task">
-                ①active user
-              </div>
-            </li>
-            <li className="nav-item">
-              <div className="nav-link">
-                ②disabled user
-              </div>
-            </li>
+            {this.props.other_users.map( (other_user, index) => {
+              if ( index == 0 ){
+                return (
+                  <li className="nav-item show">
+                    <div className="nav-link task" key={index}>
+                      {other_user.nickname}
+                    </div>
+                  </li>
+                )
+              } else {
+                return (
+                  <li className="nav-item">
+                    <div className="nav-link task" key={index}>
+                      {other_user.nickname}
+                    </div>
+                  </li>
+                )
+              }
+            })}
           </ul>
         </div>
-
       </React.Fragment>
     )
   }
